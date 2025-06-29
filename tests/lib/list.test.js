91c8e3a40527db7389b29c89156adfd351c01bb0
Ashwin <ashwinabraham96@gmail.com>
2025-06-29 01:31:39 +0100

    Filter list() results to include only folders and add tests

--- tests/lib/list.test.js
+++ tests/lib/list.test.js
@@ -2,15 +2,18 @@ import { vi, beforeEach, describe, test, expect } from "vitest";
 import { list } from "../../lib/list";
 
 let readdir = vi.spyOn(await import("../../utils/fs"), "readdir");
+let isFolder = vi.spyOn(await import("../../utils/fs"), "isFolder");
 
 beforeEach(() => {
     readdir.mockClear();
+    isFolder.mockClear();
 });
 
 describe("Normal run", () => {
     test("Should list directory contents", async () => {
         const mockDirectoryContents = ["file1.json", "file2.json"];
         readdir.mockImplementation(() => mockDirectoryContents);
+        isFolder.mockImplementation(() => true);
 
         const contents = await list();
 
@@ -22,6 +25,17 @@ describe("Normal run", () => {
 describe("Function errors", () => {
     test("Should throw error if readdir fails", async () => {
         readdir.mockImplementation(() => {
+            throw new Error("Error");
+        });
+        isFolder.mockImplementation(() => true);
+
+        await expect(list()).rejects.toThrowError("Cannot run list:Error");
+    });
+
+    test("Should throw error if isFolder fails", async () => {
+        const mockDirectoryContents = ["file1.json", "file2.json"];
+        readdir.mockImplementation(() => mockDirectoryContents);
+        isFolder.mockImplementation(() => {
             throw new Error("Error");
         });
 
