550be522288fdb0f8e1affa67cae1db4f313fef4
Ashwin <ashwinabraham96@gmail.com>
2025-07-14 23:52:46 +0100

    Update version to 0.0.9 and set default FS env path per OS

--- utils/env.js
+++ utils/env.js
@@ -1,9 +1,39 @@
 import "dotenv/config";
 
+import os from "os";
+import path from "path";
+
+function getAppDataPath() {
+    const homeDir = os.homedir();
+    const appName = "FacesDB";
+
+    switch (process.platform) {
+        case "darwin":
+            return path.join(
+                homeDir,
+                "Library",
+                "Application Support",
+                appName,
+            );
+        case "win32":
+            return path.join(
+                process.env.APPDATA || path.join(homeDir, "AppData", "Roaming"),
+                appName,
+            );
+        case "linux":
+        default:
+            return path.join(
+                process.env.XDG_DATA_HOME ||
+                    path.join(homeDir, ".local", "share"),
+                appName,
+            );
+    }
+}
+
 export function env(part) {
     switch (part.toUpperCase()) {
         case "FS":
-            return process.env.FS || "";
+            return process.env.FS || getAppDataPath();
         case "PAGESIZE":
             return parseInt(process.env.PAGESIZE) || 10;
     }
